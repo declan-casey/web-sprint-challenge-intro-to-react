21cf1ab50046df64e700c0284ca46401d3b34458
declan-casey <dpcaseyboy99@gmail.com>
2020-12-11 13:26:35 -0500

    done

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components"
+
+const StyledHeader = styled.div`
+    
+`
+function Header() {
+    return (
+        <StyledHeader>
+            <h1>List Of Targets</h1>
+        </StyledHeader>
+    )
+}
+
+export default Header
